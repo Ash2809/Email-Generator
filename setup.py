5b9f84ba871359db9de2952ae7c3da41cebaccde
Ash2809 <aashutoshkumar2809@gmail.com>
2024-07-18 16:56:35 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Email Generator',
+    version= '0.0.1',
+    author= 'Aashutosh Kumar',
+    author_email= 'aashutoshkumar2809@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
